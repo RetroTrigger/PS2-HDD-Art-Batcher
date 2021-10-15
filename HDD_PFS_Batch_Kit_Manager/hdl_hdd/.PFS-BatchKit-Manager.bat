0924509a9592e10993f04bf055aa448e7dc9e088
GDX-X <GDX2ATV@hotmail.fr>
2021-10-15 22:40:32 +0200

    Update .PFS-BatchKit-Manager.bat

--- HDD_PFS_Batch_Kit_Manager/hdl_hdd/.PFS-BatchKit-Manager.bat
+++ HDD_PFS_Batch_Kit_Manager/hdl_hdd/.PFS-BatchKit-Manager.bat
@@ -22,7 +22,8 @@ if '%errorlevel%' NEQ '0' (
 
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
